d1f2bd38d59fa7e22ee3e71cc76095f22ebb0780
Mahdi Rafati <mahdirafati680@gmail.com>
2023-11-21 15:42:42 +0330

    Progress bar added

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def start_download():
     try:
         # Creating YouTube object by the given link
         yt_link = link.get()
-        yt_object = YouTube(yt_link)
+        yt_object = YouTube(yt_link, on_progress_callback=on_progress)
         video = yt_object.streams.get_highest_resolution()
         # Showing the title of the video
         title.configure(text=yt_object.title, text_color='white')
@@ -18,6 +18,13 @@ def start_download():
         print(err)
     else:
         state_label.configure(text="Download Completed Successfully!", text_color="green")
+
+def on_progress(stream, chunk, bytes_remaining):
+    total_size = stream.filesize
+    bytes_downloaded = total_size - bytes_remaining
+    percentage_of_compeletion = int(bytes_downloaded / total_size * 100)
+    progress_bar.configure(text=str(percentage_of_compeletion) + '%')
+    progress_bar.set(percentage_of_compeletion / 100)
 
 # System Setting
 customtkinter.set_appearance_mode("dark")
@@ -41,6 +48,11 @@ link.pack()
 # State Label
 state_label = customtkinter.CTkLabel(app, text="")
 state_label.pack()
+
+# Progress Bar
+progress_bar = customtkinter.CTkProgressBar(app, progress_color='blue')
+progress_bar.set(0)
+progress_bar.pack(padx=10, pady=10)
 
 # Download Button
 download = customtkinter.CTkButton(app, text="Download", command=start_download)
