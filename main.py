2f9d9bf1ad27aca4a913c7cdd73e0f78d6332c80
Mahdi Rafati <mahdirafati680@gmail.com>
2023-11-23 23:28:04 +0330

    Option menu added

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
-import tkinter as tk
-import customtkinter
 from pytube import YouTube
 from pytube.exceptions import RegexMatchError
 from urllib.error import URLError
+import customtkinter
 
 def start_download() -> None:
     """Start downloading YouTube video by given link"""
-    state_label.configure(text="Donwloading...")
+    state_label.configure(text="Donwloading...", text_color='white')
     try:
         # Creating YouTube object by the given link
         yt_link = link.get()
         yt_object = YouTube(yt_link, on_progress_callback=on_progress)
-        video = yt_object.streams.get_highest_resolution()
+        video = yt_object.streams.get_by_resolution(option_menu.get())
         # Showing the title of the video
         title.configure(text=yt_object.title, text_color='white')
         # Downloading the video
@@ -39,31 +38,36 @@ customtkinter.set_default_color_theme("blue")
 
 # App frame
 app = customtkinter.CTk()
-app.geometry("720x480")
+app.geometry("800x600")
 app.title("YouTube Downloader")
 
 # Adding UI Elements
 # Title
 title = customtkinter.CTkLabel(app, text="Insert a YouTube link")
-title.pack(padx=10, pady=10)
+title.pack(padx=20, pady=20)
 
 # Link input 
-url = tk.StringVar()
-link = customtkinter.CTkEntry(app, width=350, height=40, textvariable=url)
+url = customtkinter.StringVar()
+link = customtkinter.CTkEntry(app, width=450, height=40, textvariable=url)
 link.pack()
 
 # State Label
 state_label = customtkinter.CTkLabel(app, text="")
-state_label.pack()
+state_label.pack(pady=5)
 
 # Progress Bar
-progress_bar = customtkinter.CTkProgressBar(app, progress_color='blue')
+progress_bar = customtkinter.CTkProgressBar(app, progress_color='blue', width=500, height=15)
 progress_bar.set(0)
-progress_bar.pack(padx=10, pady=10)
+progress_bar.pack(padx=15, pady=15)
+
+# Option Menu
+option = customtkinter.StringVar(value="480p")
+option_menu = customtkinter.CTkOptionMenu(app,values=['144p', '240p', '360p','480', '720p'], variable=option)
+option_menu.pack(pady=5)
 
 # Download Button
-download = customtkinter.CTkButton(app, text="Download", command=start_download)
-download.pack(padx=10, pady=10)
+download_btn = customtkinter.CTkButton(app, text="Download", width=150, height=35, command=start_download)
+download_btn.pack(padx=20, pady=20)
 
 #Run App
 app.mainloop()
